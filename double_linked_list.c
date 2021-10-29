ef544c89c9464edda220c4687b7238288f667f5b
raruntpm <rarun.tpm88@gmail.com>
2021-10-29 10:48:20 +0530

    Added the linked list files

--- double_linked_list.c
+++ double_linked_list.c
@@ -1,41 +1,133 @@
-/* This is the program is to demonstrate the use of the 
- * double linked list. first we create the linked list and
- *  then add the cell to the linked list....
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-
-struct arun
+/* structure representing a node of the doubly linked list */
+struct dnode
 {
-	int i;
-	char c;
-	struct arun *next;
-	struct arun *prev;
+    struct dnode *prev;
+    int data;
+    struct dnode *next;
 };
-#define NULL (void *)0
-void add (struct arun *, struct arun *);
-void main()
+
+struct dnode *start = NULL;
+
+void append(int);
+void addatbeg(int);
+void remov(int);
+void display();
+
+int main()
 {
-	int n;
-	struct arun c1,c2;
-	struct arun c3 ={ 12,'c', NULL, NULL};
-	c1.i = 10;
-	c1.c = 'a';
-	c1.next = &c2;
-	c1.prev = &c2;
-	
-	c2.i = 10;
-	c2.c = 'a';
-	c2.next = &c1;
-	c2.prev = &c1;
-	
-	add (&c3,&c2);
-//	for (n = 0; n<2; n++)
-		
+    int n, ch;
+    do
+    {
+        printf("\n\nOperations on doubly linked list");
+        printf("\n1. Append \n2. Add at beginning \n3. Remove\n4. Display\n0. Exit\n");
+        printf("\nEnter Choice 0-4? : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+            case 1:
+                printf("\nEnter number: ");
+                scanf("%d", &n);
+                append(n);
+                break;
+            case 2:
+                printf("\nEnter number: ");
+                scanf("%d", &n);
+                addatbeg(n);
+                break;
+            case 3:
+            	printf("\nEnter number to delete: ");
+                scanf("%d", &n);
+                remov(n);
+                break;
+            case 4:
+                display();
+                break;
+        }
+    }while (ch != 0);
 }
-void add (struct arun *new, struct arun *old)
+
+/* adds a new node at the end of the doubly linked list */
+void append(int num)
 {
-	new -> next = old ->next;
-	new -> prev = old;
-	old ->next ->prev = new;
-	old ->next =new;
+    struct dnode *nptr,  *temp = start;
+
+    /*create a new node */
+    nptr = malloc(sizeof(struct dnode));
+    nptr->data = num;
+    nptr->next = NULL;
+    nptr->prev = NULL;
+
+    /* if the linked list is empty */
+    if (start == NULL)
+    {
+        start = nptr;
+    } 
+    else
+    {
+        /* traverse the linked list till the last node is reached */
+        while (temp->next != NULL)
+            temp = temp->next;
+
+        nptr->prev = temp;
+        temp->next = nptr;
+    }
+}
+
+/* adds a new node at the begining of the linked list */
+void addatbeg(int num)
+{
+    struct dnode *nptr;
+
+    /* create a new node */
+    nptr = malloc(sizeof(struct dnode));
+
+    /* assign data and pointer to the new node */
+    nptr->prev = NULL;
+    nptr->data = num;
+    nptr->next = start;
+
+    if (start != NULL)
+        start->prev = nptr;
+    start = nptr;
+}
+
+
+/* deletes the specified node from the doubly linked list */
+void remov(int num)
+{
+    struct dnode *temp = start;
+
+    /* traverse the entire linked list */
+    while (temp != NULL)
+    {
+        /* if node to be deleted is found */
+        if (temp->data == num)
+        {
+            /* if node to be deleted is the first node */
+            if (temp == start)
+            {
+                start = start->next;
+                start->prev = NULL;
+            } 
+            else
+            {
+                /* if node to be deleted is the last node */
+                if (temp->next == NULL)
+                    temp->prev->next = NULL;
+                else
+                /* if node to be deleted is any intermediate node */
+                {
+                    temp->prev->next = temp->next;
+                    temp->next->prev = temp->prev;
+                }
+                free(temp);
+            }
+            return ; /* return back after deletion */
+        }
+        temp = temp->next; /* go to next node */
+    }
+    printf("\n%d not found.", num);
 }
